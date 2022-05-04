c5f417b2fb248212826664ef5e4ee3456cc9f7e9
devYeonjun <alcls0026@gmail.com>
2022-05-04 22:37:44 +1200

    Add Header & Meals

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,18 @@
 import { Fragment } from "react";
 
-import measlsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css'
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
 	return (
 		<Fragment>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
-				<button>Cart</button>
+				<HeaderCartButton />
 			</header>
-			<div className={classes['main-image']}>
-				<img src={measlsImage} alt='A table full of food'/>
+			<div className={classes["main-image"]}>
+				<img src={mealsImage} alt="A table full of delicious food!" />
 			</div>
 		</Fragment>
 	);
