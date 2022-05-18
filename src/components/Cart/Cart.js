3235a16f7114acf87430da061e61a948f02a5f90
devYeonjun <alcls0026@gmail.com>
2022-05-18 16:04:05 +1200

    Add clearcart & UI

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,6 +8,8 @@ import Checkout from "./Checkout";
 
 const Cart = (props) => {
 	const [isCheckout, setIsCheckout] = useState(false);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [didSubmit, setDidSubmit] = useState(false);
 	const cartCtx = useContext(CartContext);
 
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -25,57 +27,85 @@ const Cart = (props) => {
 		setIsCheckout(true);
 	};
 
-	const submitOrderHandler = (userData) => {
-		fetch('https://react-http-a1d35-default-rtdb.firebaseio.com/orders.json', {
-		  method: 'POST',
-		  body: JSON.stringify({
-			user: userData,
-			orderedItems: cartCtx.items
-		  })
-		});
-	  };
-	
-	  const cartItems = (
-		<ul className={classes['cart-items']}>
-		  {cartCtx.items.map((item) => (
-			<CartItem
-			  key={item.id}
-			  name={item.name}
-			  amount={item.amount}
-			  price={item.price}
-			  onRemove={cartItemRemoveHandler.bind(null, item.id)}
-			  onAdd={cartItemAddHandler.bind(null, item)}
-			/>
-		  ))}
-		</ul>
-	  );
-	
-	  const modalActions = (
-		<div className={classes.actions}>
-		  <button className={classes['button--alt']} onClick={props.onClose}>
-			Close
-		  </button>
-		  {hasItems && (
-			<button className={classes.button} onClick={orderHandler}>
-			  Order
-			</button>
-		  )}
-		</div>
-	  );
-	
-	  return (
-		<Modal onClose={props.onClose}>
-		  {cartItems}
-		  <div className={classes.total}>
-			<span>Total Amount</span>
-			<span>{totalAmount}</span>
-		  </div>
-		  {isCheckout && (
-			<Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
-		  )}
-		  {!isCheckout && modalActions}
-		</Modal>
-	  );
+	const submitOrderHandler = async (userData) => {
+		setIsSubmitting(true);
+		await fetch(
+			"https://react-http-a1d35-default-rtdb.firebaseio.com/orders.json",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					user: userData,
+					orderedItems: cartCtx.items,
+				}),
+			}
+		);
+		setIsSubmitting(false);
+		setDidSubmit(true);
+		cartCtx.clearCart()
 	};
+
+	const cartItems = (
+		<ul className={classes["cart-items"]}>
+			{cartCtx.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
+			))}
+		</ul>
+	);
+
+	const modalActions = (
+		<div className={classes.actions}>
+			<button className={classes["button--alt"]} onClick={props.onClose}>
+				Close
+			</button>
+			{hasItems && (
+				<button className={classes.button} onClick={orderHandler}>
+					Order
+				</button>
+			)}
+		</div>
+	);
+
+	const cartModalContent = (
+		<>
+			{cartItems}
+			<div className={classes.total}>
+				<span>Total Amount</span>
+				<span>{totalAmount}</span>
+			</div>
+			{isCheckout && (
+				<Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+			)}
+			{!isCheckout && modalActions}
+		</>
+	);
+
+	const isSubmittingModalContent = <p>Sending order data...</p>;
+
+	const didSubmittingModalContent = (
+		<>
+			<p>Successfully sent the order</p>
+			<div className={classes.actions}>
+				<button className={classes.button} onClick={props.onClose}>
+					Close
+				</button>
+			</div>
+		</>
+	);
+
+	return (
+		<Modal onClose={props.onClose}>
+			{!isSubmitting && !didSubmit && cartModalContent}
+			{isSubmitting && isSubmittingModalContent}
+			{!isSubmitting && didSubmit && didSubmittingModalContent}
+		</Modal>
+	);
+};
 
 export default Cart;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -55,6 +55,10 @@ const cartReducer = (state, action) => {
 		};
 	}
 
+	if (action.type === "CLEAR") {
+		return defaultCartState;
+	}
+
 	return defaultCartState;
 };
 
@@ -71,11 +75,16 @@ const CartProvider = (props) => {
 		dispatchCartAction({ type: "REMOVE", id: id });
 	};
 
+	const clearCarHandler = () => {
+		dispatchCartAction({type: 'CLEAR'})
+	}
+
 	const cartContext = {
 		items: cartState.items,
 		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
+		clearCart: clearCarHandler
 	};
 	return (
 		<CartContext.Provider value={cartContext}>
